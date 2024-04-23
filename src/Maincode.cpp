7f781962a0234ca8876ff02702a34d0127ab89a2
Mohammed Sameer Syed <sdmohammedsameer@gmail.com>
2024-04-23 21:11:36 +0530

    Update Maincode.cpp

--- src/Maincode.cpp
+++ src/Maincode.cpp
@@ -316,19 +316,7 @@ void loop()
 
 
     //TVC
-    /* Delta Space Systems
-      Version 2.31
-    September, 10th 2020   */
 
-/* Tuning Guide - README
- *  To tune the servo offsets for your TVC   Mount go to line 52. 
-    To tune just raise the values until the servo switches   directions then home in on the perfect value.
- *  To change the ratio between   the Servo and the TVC mount go to line 64. 
-    Use a seperate sketch and raise   the degree the servo moves until it hits the end of the TVC Mount. 
-    Then   divide the servo degree by 5.
- *  For PID Value Changes go to line 112 - 114.
-   *  To change what I/O pins the servos are connected to go to line 139 - 140.  
-   */
 
 
 /*System State: 
